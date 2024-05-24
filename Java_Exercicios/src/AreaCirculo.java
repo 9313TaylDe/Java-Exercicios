ec72af9f6907e81a39b8e344d2afe1af1c0d397d
TAY <TAY@gmail.com>
2024-05-24 10:27:19 -0300

    Area de um circulo

---
+++ Java_Exercicios/src/AreaCirculo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AreaCirculo {
+    public static void main(String[] args) {
+        
+     Scanner sc =  new Scanner(System.in);
+
+        System.out.print("Digite o valor do raio do circulo: ");
+        double raio = sc.nextDouble();
+        
+        double area = Math.PI * (raio * raio);
+        System.out.printf("A área de um circulo de raio %.1fr é %.2f", raio, area);
+
+    }
+}
