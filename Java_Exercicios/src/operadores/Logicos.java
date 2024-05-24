65eac54cb1f114090f5a0f57158b64d5803c16df
TAY <TAY@gmail.com>
2024-05-24 09:26:03 -0300

    Update

---
+++ Java_Exercicios/src/operadores/Logicos.java
@@ -0,0 +1,16 @@
+package operadores;
+
+public class Logicos {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 5;
+        System.out.println("Operadores lógicos são");
+        System.out.println("Operador de &&  AND, será verdadeiro somente se ambos forem verdadeiros "+( a > b && b > a));
+        System.out.println("Operador de ||  OR, será falso se ambos forem falsos "+ (a > b || b <  a));
+        System.out.println("Operador de XOR ou or exclusivo, que só será falso se ambos forem falsos ou verdadeiros "+(a < b ^ b > a));
+        System.out.println("Operador de NOT, inverterá a lógica ou seja, se for verdadeira inverterá e será falso "+ (a > b));
+    }
+
+}
