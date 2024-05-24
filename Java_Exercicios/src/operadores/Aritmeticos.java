447a1e7d78066bb7b09ef1dba94843950c3557ee
TAY <TAY@gmail.com>
2024-05-24 09:01:22 -0300

    Mais dois novos programas em linguagem java

---
+++ Java_Exercicios/src/operadores/Aritmeticos.java
@@ -0,0 +1,21 @@
+package operadores;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 10;
+        int soma = num1 + num2;
+        int multi = num1 * num2;
+        int subtr = num1 - num2;
+        double divi = num1 / num2;
+        int modu = num1 % num2;
+        System.out.println("Operadores ariméticos são");
+        System.out.println("Adição + " + soma);
+        System.out.println("Subtração - "+ subtr);
+        System.out.println("Divisão / "+ divi);
+        System.out.println("Multiplicação * "+multi);
+        System.out.println("Módulo ou resto da divisao % "+modu);
+    }
+
+}
---
+++ Java_Exercicios/src/operadores/Logicos.java
@@ -0,0 +1,19 @@
+package operadores;
+
+public class Logicos {
+    public static void main(String[] args) {
+
+        int i = 10;
+        int a = 5;
+        
+        System.out.println("Os operadores lógicos são");
+        System.out.println("Operador de maior > "+(i > a));
+        System.out.println("Operador de maior ou igual >= "+(i >= a));
+        System.out.println("Operador de menor < "+(i < a));
+        System.out.println("Operador de menor ou igual <= "+(i <= a));
+        System.out.println("Operador de igualdade == "+(i == a));
+        System.out.println("Operador de diferença != "+(i != a));
+        
+    }
+
+}
