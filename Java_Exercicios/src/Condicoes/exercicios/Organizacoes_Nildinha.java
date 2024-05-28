d56ea9817883445ffe3fe575932389cdf51c3133
9313TayIDe <tay@gmail.com>
2024-05-28 17:14:12 -0300

    Update

--- Java_Exercicios/src/Condicoes/exercicios/Organizacoes_Nildinha.java
+++ Java_Exercicios/src/Condicoes/exercicios/Organizacoes_Nildinha.java
@@ -18,7 +18,8 @@ public class Organizacoes_Nildinha {
             int percentual = 20 ;
             System.out.printf("Seu antigo salário era de R$%.2f", antigo_salario);
             System.out.printf("\nO percentual de aumento é de %d%%", percentual);
-            System.out.printf("\n$s o seu novo salário com auemnto de 20%% será de R$%.2f", (salario + aumento_vinte));
+            System.out.printf("\nO valor do aumento R$%.2f", aumento_vinte);
+            System.out.printf("\n%s o seu novo salário com auemnto de 20%% será de R$%.2f", nome,(salario + aumento_vinte));
         }
         else if(salario > 280 && salario <= 700){
             double aumento_quinze = (salario * 15)/100;
@@ -26,7 +27,8 @@ public class Organizacoes_Nildinha {
             int percentual = 15 ;
             System.out.printf("Seu antigo salário era de R$%.2f", antigo_salario);
             System.out.printf("\nO percentual de aumento é de %d%%", percentual);
-            System.out.printf("\n$s o seu novo salário com auemnto de 15%% será de R$%.2f", (salario + aumento_quinze));
+            System.out.printf("\nO valor do aumento R$%.2f", aumento_quinze);
+            System.out.printf("\n%s o seu novo salário com auemnto de 15%% será de R$%.2f", nome,(salario + aumento_quinze));
         }
         else if(salario > 700 && salario <= 1500){
             double aumento_dez = (salario * 10)/100;
@@ -34,7 +36,8 @@ public class Organizacoes_Nildinha {
             int percentual = 10;
             System.out.printf("Seu antigo salário era de R$%.2f", antigo_salario);
             System.out.printf("\nO percentual de aumento é de %d%%", percentual);
-            System.out.printf("\n$s o seu novo salário com auemnto de 10%% será de R$%.2f", (salario + aumento_dez));
+            System.out.printf("\nO valor do aumento R$%.2f", aumento_dez);
+            System.out.printf("\n%s o seu novo salário com auemnto de 10%% será de R$%.2f", nome,(salario + aumento_dez));
         }
         else if(salario > 1500){
             double aumento_cinco = (salario * 5)/100;
@@ -42,7 +45,8 @@ public class Organizacoes_Nildinha {
             int percentual = 5 ;
             System.out.printf("Seu antigo salário era de R$%.2f", antigo_salario);
             System.out.printf("\nO percentual de aumento é de %d%%", percentual);
-            System.out.printf("\n$s o seu novo salário com auemnto de 5%% será de R$%.2f", (salario + aumento_cinco));
+            System.out.printf("\nO valor do aumento R$%.2f", aumento_cinco);
+            System.out.printf("\n%s o seu novo salário com auemnto de 5%% será de R$%.2f", nome,(salario + aumento_cinco));
         }
 
         return salario;
