e7fca70d9df12bb8103aa522822647a39eb69d88
Expedito <Expedito@gmail.com>
2024-06-03 10:30:39 -0300

    Posto de gasolina

---
+++ Java_Exercicios/src/Condicoes/exercicios/Combustiveis.java
@@ -0,0 +1,16 @@
+package Condicoes.exercicios;
+
+import java.util.Scanner;
+
+public class Combustiveis {
+
+    public static void Posto() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(" --BEM-VINDO-AO-POSTO-SÃO-DOMINGOS-- ");
+
+        double preco = 0.0;
+
+    }
+
+}
