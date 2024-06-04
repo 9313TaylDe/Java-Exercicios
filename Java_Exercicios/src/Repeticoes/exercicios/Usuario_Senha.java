86bdd94f3ec081c048a9f264b3c5695c775f9894
Expedito <irospidermanstronger@gmail.com>
2024-06-04 17:44:35 -0300

    Programa que pede o usuario e senha para login.

---
+++ Java_Exercicios/src/Repeticoes/exercicios/Usuario_Senha.java
@@ -0,0 +1,41 @@
+package Repeticoes.exercicios;
+
+import java.util.Scanner;
+
+public class Usuario_Senha {
+
+    public static void User() {
+        Scanner sc = new Scanner(System.in);
+
+        boolean usuarioAtivo = false;
+
+        while (!usuarioAtivo) {
+            System.out.print("USUÁRIO: ");
+            String usuario = sc.next();
+
+            System.out.print("SENHA: ");
+            String senha = sc.next();
+
+            String valida_user = usuario;
+            String valida_senha = senha;
+            if (!usuario.equalsIgnoreCase("expedito") && !senha.equalsIgnoreCase("12345")) {
+                System.out.flush();
+                System.out.println("\nUsuario e senha incorretos");
+            } else if (usuario.equals(senha)) {
+                System.out.flush();
+                System.out.println("\nSenha não poe ser igual ao usuario!");
+            } else {
+                System.out.flush();
+                System.out.print("\nLogin bem sucedido");
+                break;
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        User();
+    }
+
+}
