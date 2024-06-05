930bb0720543876f5a3b8439db703caf0b404e5b
Expedito <irospidermanstronger@gmail.com>
2024-06-05 14:28:52 -0300

    update the interval program.

--- Java_Exercicios/src/Repeticoes/exercicios/Gerando_intervalo.java
+++ Java_Exercicios/src/Repeticoes/exercicios/Gerando_intervalo.java
@@ -7,12 +7,19 @@ public class Gerando_intervalo {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite um número menor: ");
         int menor = sc.nextInt();
+        int i = 0;
+        int soma = 0;
 
         System.out.print("Digite um maior: ");
         int maior = sc.nextInt();
-        for (int i = menor + 1; i < maior; i++) {
+        for ( i = menor + 1; i <= maior; i++) {
             System.out.printf("\nNúmeros no intervalo %d", i);
         }
+
+        for(i = menor; i <= maior; i ++){
+            soma += i;
+        }
+        System.out.printf("\nA soma dos números do intervalo é de %d",soma);
 
     }
 
--- Java_Exercicios/src/Repeticoes/exercicios/Listando_numeros.java
+++ Java_Exercicios/src/Repeticoes/exercicios/Listando_numeros.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 
 public class Listando_numeros {
 
-    public static void Listando()
-    {
+    public static void Listando() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite um número: ");
         int num = sc.nextInt();
-        for(int i = 1; i <= num; i ++){
-            System.out.printf("%d - ",i);
+        for (int i = 1; i <= num; i++) {
+            System.out.printf("%d - ", i);
         }
-    
-    }
 
-  
+    }
 
     public static void main(String[] args) {
         Listando();
