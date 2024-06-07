cca140910d963372c432bcc19dda9fe742d1b2bb
Expedito <irospidermanstronger@gmail.com>
2024-06-06 21:12:10 -0300

    Update

---
+++ Java_Exercicios/src/Condicoes/Posto_Gasolina.java
@@ -0,0 +1,10 @@
+package Condicoes;
+
+import java.util.Scanner;
+
+public class Posto_Gasolina {
+    public static void Posto(){
+        Scanner sc = new Scanner(System.i)
+    }
+
+}
--- Java_Exercicios/src/Condicoes/exercicios/Combustiveis.java
+++ Java_Exercicios/src/Condicoes/exercicios/Combustiveis.java
@@ -1,5 +1,3 @@
-package Condicoes.exercicios;
-
 import java.util.Scanner;
 
 public class Combustiveis {
@@ -7,60 +5,51 @@ public class Combustiveis {
     public static void Posto() {
         Scanner sc = new Scanner(System.in);
         double desconto = 0;
-        System.out.print(" --BEM-VINDO-AO-POSTO-SÃO-DOMINGOS------ ");
-        System.out.print("Preço da gasolina: R$2,50 | Preço do alcool: R$1,90");
-        System.out.print(
-                "\n--------------------------------------------------------------------------------------------\n");
+        System.out.println(" --BEM-VINDO-AO-POSTO-SÃO-DOMINGOS------ ");
+        System.out.println("Preço da gasolina: R$2,50 | Preço do álcool: R$1,90");
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
         System.out.print("Quanto de gasolina? ");
 
         double quantidade_gasolina = sc.nextDouble();
-        System.out.print("Quanto de alcool? ");
-
+        System.out.print("Quanto de álcool? ");
         double quantidade_alcool = sc.nextDouble();
-        double valor_alcool = 1.90;
-        double valor_gasoline = 2.50;
-        String alcool = "";
-        System.out.print("Aditivada ou comum? ");
-        String gasolina = sc.next();
-        System.out.println("\n");
-        if (gasolina.equalsIgnoreCase("aditivada")) {
-            System.out.printf("Tipo de gasolina %s", gasolina);
-            System.out.print("\n-----------------------------");
-            if (quantidade_gasolina <= 20) {
-                desconto = quantidade_gasolina * 0.06;
-                System.out.printf("\nValor a ser pago pela gasolina: R$%.2f", quantidade_gasolina - desconto);
-                System.out.printf("\nDesconto na compra da gasolina R$%.2f", desconto);
-            } else if (quantidade_gasolina >= 20) {
-                desconto = quantidade_gasolina * 0.05;
-                System.out.printf("\nValor a ser pago pela gasolina: R$%.2f", quantidade_gasolina - desconto);
-                System.out.printf("\nDesconto na compra da gasolina R$%.2f", desconto);
-            }
-        }
-        else if (gasolina.equalsIgnoreCase("comum")) {
-            System.out.printf("Tipo de gasolina %s", gasolina);
-            System.out.print("\n-----------------------------");
-            if (quantidade_gasolina >= 20) {
-                desconto = quantidade_gasolina * 0.06;
-                System.out.printf("\nValor a ser pago pela gasolina: R$%.2f", quantidade_gasolina - desconto);
-                System.out.printf("\nDesconto na compra da gasolina R$%.2f", desconto);
-            } else if (quantidade_gasolina <= 20) {
-                desconto = quantidade_gasolina * 0.05;
-                System.out.printf("\nValor a ser pago pela gasolina: R$%.2f", quantidade_gasolina - desconto);
-                System.out.printf("\nDesconto na compra da gasolina R$%.2f", desconto);
 
+        double valor_final_gasolina = 0;
+        double valor_final_alcool = 0;
+
+        double preco_gasolina = 2.50;
+        double preco_alcool = 1.90;
+
+        double desconto_gasolina = 0.04;
+        double desconto_alcool = 0.03;
+        System.out.print("Aditivada ou comum? ");
+        String tipo_gasolina = sc.next();
+
+        if (tipo_gasolina.equalsIgnoreCase("aditivada")) {
+            if (quantidade_gasolina <= 20) {
+                valor_final_gasolina = quantidade_gasolina * preco_gasolina;
+            } else {
+                valor_final_gasolina = quantidade_gasolina * preco_gasolina * (1 - desconto_gasolina);
+            }
+        } else if (tipo_gasolina.equalsIgnoreCase("comum")) {
+            if (quantidade_gasolina <= 20) {
+                valor_final_gasolina = quantidade_gasolina * preco_gasolina;
+            } else {
+                valor_final_gasolina = quantidade_gasolina * preco_gasolina * (1 - desconto_gasolina);
             }
         }
-        System.out.println("\n-------------------------------------------------------");
 
         if (quantidade_alcool <= 20) {
-            desconto = quantidade_alcool * 0.03;
-            System.out.printf("\nValor a ser pago pelo álcool: R$%.2f", quantidade_alcool - desconto);
-            System.out.printf("\nDesconto na compra do álcool R$%.2f", desconto);
+            valor_final_alcool = quantidade_alcool * preco_alcool;
         } else {
-            desconto = quantidade_alcool * 0.05;
-            System.out.printf("\nValor a ser pago pelo álcool: R$%.2f", quantidade_alcool - desconto);
-            System.out.printf("\nDesconto na compra do álcool R$%.2f", desconto);
+            valor_final_alcool = quantidade_alcool * preco_alcool * (1 - desconto_alcool);
         }
+
+        double valor_total = valor_final_gasolina + valor_final_alcool;
+        System.out.printf("Valor a ser pago pela gasolina: R$%.2f%n", valor_final_gasolina);
+        System.out.printf("Valor a ser pago pelo álcool: R$%.2f%n", valor_final_alcool);
+        System.out.printf("Valor total a ser pago: R$%.2f%n", valor_total);
     }
 
     public static void main(String[] args) {
