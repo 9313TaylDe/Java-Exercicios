2eb76cc60cb835cedb9ec083b2747048ca76742e
Expedito <irospidermanstronger@gmail.com>
2024-06-07 09:35:45 -0300

    Impares e pares

--- Java_Exercicios/src/Repeticoes/exercicios/Impares_e_Pares.java
+++ Java_Exercicios/src/Repeticoes/exercicios/Impares_e_Pares.java
@@ -4,27 +4,46 @@ import java.util.Scanner;
 
 public class Impares_e_Pares {
     public static void ImparesPares() {
+        Scanner sc = new Scanner(System.in);
+        int numeros[] = new int[10];
+        int i = 0;
+        int impares = 0;
+        int pares = 0;
+        for (i = 0; i < 10; i++) {
+            System.out.printf("Digite o %dº número: ", i);
+            numeros[i] = sc.nextInt();
+        }
+        for (int num : numeros) {
+            if (num % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+        System.out.printf("Quantidade de pares:  %d", pares);
+        System.out.printf("\nQuantidade de ímpares: %d", impares);
+    }
 
+    public static void ParesImpares() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite um inicio: ");
+        System.out.print("Digite o inicio: ");
         int inicio = sc.nextInt();
-
-        System.out.print("Digite o ultimo número a ser mostrado: ");
+        int impares = 0;
+        int pares = 0;
+        System.out.print("Digite o final: ");
         int parada = sc.nextInt();
 
-        for (int i = inicio; i < parada; i++) {
+        for (int i = inicio; i < parada + 1; i++) {
             if (i % 2 == 0) {
-                System.out.printf("\nPares: %d", i);
+                System.out.printf("\nPares %d", i);
+            } else {
+                System.out.printf("\nímpares %d", i);
             }
-            if (i % 2 != 0) {
-                System.out.printf("\nÍmpares: %d", i);
-            }
-
         }
     }
 
     public static void main(String[] args) {
-        ImparesPares();
+        ParesImpares();
     }
 }
